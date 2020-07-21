71fe7a6901d294fdc1e15ba8e5202ceee49cd108
Ryan Ross <ryguyeditor@gmail.com>
2020-07-20 21:03:22 -0700

    build

---
+++ public/build/precache-manifest.b05eafb019315d98ad73c9cc548ce670.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d77b425ec2a42e0cce612ec9b85f7532",
+    "url": "/index.html"
+  },
+  {
+    "revision": "76d73376cc3f68e6dd90",
+    "url": "/static/css/main.87ded60d.chunk.css"
+  },
+  {
+    "revision": "00a84b41a2ff93fddd3b",
+    "url": "/static/js/2.c99c3191.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/static/js/2.c99c3191.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76d73376cc3f68e6dd90",
+    "url": "/static/js/main.6f55a385.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "e2ca3556b33bbb35a64bf5d09b9bb957",
+    "url": "/static/media/desktop-banner.e2ca3556.jpeg"
+  },
+  {
+    "revision": "0b97a32b6f71e476b17f4b392787da20",
+    "url": "/static/media/mobile-banner.0b97a32b.jpeg"
+  }
+]);
